9e8fb2b93491c3273d664bfef644519823a98408
Armando Beato Chang <armadobeatochang@gmail.com>
2025-07-10 07:16:23 -0300

    cambio react

--- index.tsx
+++ index.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { SpeedInsights } from 'https://esm.sh/@vercel/speed-insights@1.2.0/react';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-    throw new Error("Could not find root element to mount to");
-}
+    import React from 'react';
+    import ReactDOM from 'react-dom/client';
+    import App from './App';
+    import { SpeedInsights } from 'https://esm.sh/@vercel/speed-insights@1.2.0/react';
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-    <React.StrictMode>
-        <App />
-        <SpeedInsights />
-    </React.StrictMode>
-);
+    const rootElement = document.getElementById('root');
+    if (!rootElement) {
+        throw new Error("Could not find root element to mount to");
+    }
+
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+        <React.StrictMode>
+            <App />
+            <SpeedInsights />
+        </React.StrictMode>
+    );
