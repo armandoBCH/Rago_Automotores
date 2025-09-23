f7d793c1c90e34b2eadd615a149d547204a3343a
Armando Beato Chang <armadobeatochang@gmail.com>
2025-09-23 15:03:19 -0300

    f

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { FavoritesProvider } from './components/FavoritesProvider';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
